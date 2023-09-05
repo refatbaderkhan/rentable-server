8b983a14dabdfc162aa4058d2702b509b1f097c7
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-05 21:33:05 +0300

    added authentication routes to routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+const authController = require("../controllers/auth.controllers");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); 
+  },
+  filename: function (req, file, cb) {
+    const originalFilename = file.originalname;
+    cb(null, originalFilename);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+router.post("/login", authController.login)
+router.post("/register", upload.single("profile_picture"), authController.register);
+
+module.exports = router;
