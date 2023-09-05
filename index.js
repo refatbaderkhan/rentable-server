9c1cf67cd1e1526ed6dd8be5ccdaa3a9147593cf
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-05 21:39:10 +0300

    added body-parser, path and cors to index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-const express = require("express")
-const app = express();
-require("dotenv").config()
-const mongooseConnect = require("./configs/mongoDB.connect");
-app.use(express.json())
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require('path');
+const cors = require("cors");
 
-app.listen(8000, (err)=>{
-  if(err){
-      console.error(err)
-      return
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use(cors());
+app.use(express.json());
+
+const mongooseConnect = require("./configs/mongoDB.connect");
+require("dotenv").config();
+
+const PORT = 8000;
+app.listen(PORT, (err) => {
+  if (err) {
+    console.error(err);
+    return;
   }
-  console.log("server running on port: ", 8000)
-  mongooseConnect()
-})
+  console.log(`Server is running on port: ${PORT}`);
+  mongooseConnect();
+});
