ad9fa65d79ce545d2b1ef1f3483442cdcb911a2b
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-12 21:32:36 +0300

    added createSubCategory API to admin category controller

--- controllers/admin.category.controller.js
+++ controllers/admin.category.controller.js
@@ -27,7 +27,45 @@ const createCategory = async (req, res)=>{
 };
 
 
+const createSubCategory = async (req, res)=>{
+  const {category_id} = req.params;
+  const {subCategory_name} = req.body;
+
+  try {
+    if (!category_id || !subCategory_name) {
+      return res.status(400).send("Category ID and subcategory name are required.");
+    }
+
+    const category = await Category.findById(category_id);
+
+    if (!category) {
+      return res.status(404).send("Category not found.");
+    }
+
+    const existingSubCategory = category.subCategorySchema.find(
+      subcategory => subcategory.subCategory_name === subCategory_name
+    );
+
+    if (existingSubCategory) {
+      return res.status(400).send("subCategory already exists.");
+    }
+
+    const subcategory = new SubCategory({subCategory_name, category_id });
+
+    category.subCategorySchema.push(subcategory);
+    category.subCategories_names.push(subCategory_name);
+
+    await Promise.all([subcategory.save(), category.save()]);
+
+    res.status(201).send({ subcategory, message: "Subcategory created successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while creating the subcategory.");
+  }
+}
+
 module.exports = {
   createCategory,
+  createSubCategory
 }
 
