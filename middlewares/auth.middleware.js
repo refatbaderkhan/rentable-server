25700ff7a52f4ba2c2c6921fc51aa3d697dd7aad
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-12 21:24:14 +0300

    updated authentication middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,10 +1,11 @@
 const jwt = require("jsonwebtoken");
 
+
 const authMiddleware = (req, res, next) => {
     const token = req.headers.authorization?.split(" ")[1];
     if(!token) return res.status(401).send({message: "Unauthorized"});
-    try {
 
+    try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded
         next()
@@ -14,5 +15,6 @@ const authMiddleware = (req, res, next) => {
         res.status(401).send({message: "Unauthorized"})
     }
 }
+
 
 module.exports=authMiddleware;
