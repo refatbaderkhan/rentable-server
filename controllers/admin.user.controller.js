e5b0cf6fb2dfb75f9b8a2d758a9e45a872bfa53f
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-12 21:44:22 +0300

    added admin user controller to controllers

---
+++ controllers/admin.user.controller.js
@@ -0,0 +1,6 @@
+const {Category, SubCategory, Area, City} = require("../models/item.model")
+const User = require("../models/user.model")
+const Item = require("../models/item.model")
+const bcrypt = require("bcrypt");
+
+
