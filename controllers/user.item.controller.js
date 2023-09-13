81b8e0c2381a38f90972777983f5a1e09d2f7331
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:12:02 +0300

    added createItem API to user item controller

--- controllers/user.item.controller.js
+++ controllers/user.item.controller.js
@@ -20,6 +20,70 @@ const getUserItems = async (req, res)=>{
 }
 
 
+const createItem = async (req, res)=>{
+  const {
+    item_name,
+    item_description,
+    item_price,
+    item_category,
+    item_subcategory,
+    item_location
+  }= req.body;
+  console.log(req.body);
+  const item_images = req.file ? [req.file.path] : [];
+  const {username, _id, profile_picture} = req.user;
+
+  try {
+
+   const [checkCategory, checkSubcategory, checkCity, checkArea] = await Promise.all([
+     Category.findOne({category_name: item_category}),
+     SubCategory.findOne({subCategory_name: item_subcategory}),
+    City.findOne({city_name: item_location.city}),
+    City.findOne({areas: {$in: [item_location.area]}}),
+   ]);
+
+    if (!checkCategory || !checkSubcategory || !checkCity || !checkArea) {
+      return res.status(400).send("Invalid category, subcategory");
+    }
+
+    const item = new Item({
+      item_name,
+      item_description,
+      item_price,
+      item_images,
+      item_category : checkCategory._id,
+      item_category_name: item_category,
+      item_subcategory : checkSubcategory._id,
+      item_subcategory_name: item_subcategory,
+      item_location : {
+        city: checkCity._id,
+        city_name: item_location.city,
+        area: item_location.area,
+        latitude: item_location.latitude,
+        longitude: item_location.longitude,
+      },
+      username,
+      user_id: _id,
+      user_profile_picture: profile_picture,
+    });
+
+    await item.save();
+
+    await User.findByIdAndUpdate(_id, {
+      $push: {
+        user_items: item._id,
+      },
+    });
+
+    res.status(201).send({item, message: "Item created successfully."});
+  } catch (error) {
+  console.log(error);
+  res.status(500).send("An error occurred while creating the item.");
+  }
+}
+
+
 module.exports = {
-  getUserItems
+  getUserItems,
+  createItem
 }
