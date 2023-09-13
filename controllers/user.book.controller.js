847f414e771699039fbea0933306a6ebd9b8db8b
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:15:46 +0300

    added getBookings API to user book controller

--- controllers/user.book.controller.js
+++ controllers/user.book.controller.js
@@ -1,2 +1,24 @@
 const User = require("../models/user.model")
 const {Item} = require("../models/item.model")
+
+
+const getBookings = async (req, res)=>{
+  const {user_id} = req.params;
+
+  try {
+    const user = await User.findById(user_id).populate("user_bookings.item_id");
+
+    if (!user) {
+      return res.status(404).send("User not found.");
+    }
+
+    res.status(200).send(user.user_bookings);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while getting the bookings.");
+  }
+}
+
+module.exports = {
+  getBookings
+}
