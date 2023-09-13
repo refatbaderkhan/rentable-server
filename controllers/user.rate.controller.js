adad516000f3ce84df30b522fb80a36829d2559d
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:00:28 +0300

    added deleteItemRating API to user rate controller

--- controllers/user.rate.controller.js
+++ controllers/user.rate.controller.js
@@ -89,7 +89,58 @@ const modifyItemRating = async (req, res) => {
 };
 
 
+const deleteItemRating = async (req, res) => {
+  const { item_id, type, rating_id } = req.params;
+  const { _id: user_id } = req.user;
+
+  try {
+    const item = await Item.findById(item_id);
+
+    if (!item) {
+      return res.status(404).send("Item not found.");
+    }
+
+    if (type !== "rating" && type !== "review") {
+      return res.status(400).send("Invalid type.");
+    }
+
+    const ratingObject = item.item_ratings.find((rating) => rating._id.toString() === rating_id);
+
+    if (!ratingObject) {
+      return res.status(404).send("Rating not found.");
+    }
+
+    if (ratingObject.user_id.toString() !== user_id) {
+      return res.status(401).send("You are not authorized to delete this rating.");
+    }
+
+    if (type === "rating" && ratingObject.rating === null) {
+      return res.status(400).send("No rating exisits to delete.");
+    } else if (type === "review" && ratingObject.review === null) {
+      return res.status(400).send("No review exisits to delete.");
+    }
+
+    ratingObject[type] = null;
+
+    if (ratingObject.rating === null && ratingObject.review === null) {
+      const ratingIndex = item.item_ratings.findIndex((rating) => rating._id.toString() === rating_id);
+
+    if (ratingIndex === -1) {
+      item.item_ratings.pull(ratingIndex);
+    }
+  }
+
+    await item.save();
+    res.status(200).send({ item, message: `${type} deleted successfully` });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while deleting the rating.");
+  }
+};
+
+
 module.exports = {
   rateItem,
-  modifyItemRating
+  modifyItemRating,
+  deleteItemRating
 }
