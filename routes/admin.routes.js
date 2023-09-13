a33c0303e1ef30c5415e053a5375cbce23f01954
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 08:51:17 +0300

    added admin category controller to admin routes

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -9,5 +9,10 @@ router.post("/update-password/:user_id", adminUserController.updateUserPassword)
 router.delete("/delete-account/:user_id", adminUserController.deleteUserAccount);
 
 
+router.post("/create-category", adminCategoryController.createCategory);
+router.post("/create-subcategory/:category_id", adminCategoryController.createSubCategory);
+router.delete("/delete-category/:category_id", adminCategoryController.deleteCategory);
+router.delete("/delete-subcategory/:subCategory_id", adminCategoryController.deleteSubCategory);
+
 
 module.exports = router;
