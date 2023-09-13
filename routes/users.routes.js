41d9a66a1bdeb62ac35fed7675563146d512d571
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:23:53 +0300

    added multer to users routes

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,7 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const usersController = require("../controllers/users.controllers");
+const userAccountController = require("../controllers/user.account.controller");
+const userBookingController = require("../controllers/user.book.controller");
+const userFavoriteController = require("../controllers/user.favorite.controller");
+const userItemController = require("../controllers/user.item.controller");
+const userRateController = require("../controllers/user.rate.controller");
+const multer = require("multer");
 
-router.get("/", usersController.test)
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); 
+  },
+  filename: function (req, file, cb) {
+    const item_name = req.body.item_name;
+    const timestamp = new Date().toISOString().split('.')[0].replace(/:/g, "-");
+    const originalName = file.originalname;
+    const extension = originalName.split(".").pop();
+    const filename = `${item_name}${timestamp}.${extension}`;
+    cb(null, filename);
+  }
+});
+
+
+const upload = multer({ storage: storage });
+
+
 
 module.exports = router;
