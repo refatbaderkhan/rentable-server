1bc46f138ecb838ef7b77c64a1431de397e3dec7
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:11:02 +0300

    added getUserItems API to user item controller

--- controllers/user.item.controller.js
+++ controllers/user.item.controller.js
@@ -1,2 +1,25 @@
 const User = require("../models/user.model")
 const {Item, Category, SubCategory, City} = require("../models/item.model")
+
+
+const getUserItems = async (req, res)=>{
+  const {user_id} = req.params;
+
+  try {
+    const user = await User.findById(user_id).populate("user_items");
+
+    if (!user) {
+      return res.status(404).send("User not found.");
+    }
+
+    res.status(200).send(user.user_items);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred while getting the user's items.");
+  }
+}
+
+
+module.exports = {
+  getUserItems
+}
