2eb52db62f9762fe1b1fde920cbaa0d294aa2e50
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:04:44 +0300

    added deleteUserRating API to user rate controller

--- controllers/user.rate.controller.js
+++ controllers/user.rate.controller.js
@@ -224,10 +224,61 @@ const modifyUserRating = async (req, res) => {
 };
 
 
+const deleteUserRating = async (req, res) => {
+  const { user_id, type, rating_id } = req.params;
+  const { _id: rater_user_id } = req.user;
+
+  try {
+    const user = await User.findById(user_id);
+    
+    if(!user) {
+      return res.status(404).send("User not found.");
+    }
+
+    if (type !== "rating" && type !== "review") {
+      return res.status(400).send("Invalid type.");
+    }
+
+    const ratingObject = user.user_ratings.find((rating) => rating._id.toString() === rating_id);
+    
+    if (!ratingObject) {
+      return res.status(404).send("Rating not found.");
+    }
+
+    if (ratingObject.rater_user_id.toString() !== rater_user_id) {
+      return res.status(401).send("You are not authorized to delete this rating.");
+    }
+
+    if (type === "rating" && ratingObject.rating === null) {
+      return res.status(400).send("No rating exisits to delete.");
+    } else if (type === "review" && ratingObject.review === null) {
+      return res.status(400).send("No review exisits to delete.");
+    }
+
+    ratingObject[type] = null;
+
+    if (ratingObject.rating === null && ratingObject.review === null) {
+      const ratingIndex = user.user_ratings.findIndex((rating) => rating._id.toString() === rating_id);
+
+      if (ratingIndex === -1) {
+        user.user_ratings.pull(ratingIndex);
+      }
+    }
+
+      await user.save();  
+      return res.status(200).send({ message: "Rating/review and document deleted successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while deleting the rating.");
+  }
+};
+
+
 module.exports = {
   rateItem,
   modifyItemRating,
   deleteItemRating,
   rateUser,
-  modifyUserRating
+  modifyUserRating,
+  deleteUserRating,
 }
