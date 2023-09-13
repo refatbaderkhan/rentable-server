bf91a478a753df7212ba668c39793c2aa2ffefa2
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 08:49:00 +0300

    added admin routes to routes

---
+++ routes/admin.routes.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const adminUserController = require("../controllers/admin.user.controller");
+const adminLocationController = require("../controllers/admin.location.controller");
+const adminCategoryController = require("../controllers/admin.category.controller");
+
+
+module.exports = router;
