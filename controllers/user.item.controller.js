560ff9d39b9bc44934d4697c2c9e9f5a0d896c0f
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:12:41 +0300

    added modifyItem API to user item contorller

--- controllers/user.item.controller.js
+++ controllers/user.item.controller.js
@@ -83,7 +83,73 @@ const createItem = async (req, res)=>{
 }
 
 
+const modifyItem = async (req, res)=>{
+  const {item_id} = req.params;
+  const {item_name, item_description, item_price, item_category, item_subcategory, item_location} = req.body;
+  const {_id: user_id} = req.user;
+
+  try {
+    const item = await Item.findById(item_id);
+
+    if (!item) {
+      return res.status(404).send("Item not found.");
+    }
+
+    if (item.user_id.toString() !== user_id) {
+      return res.status(401).send("You are not authorized to modify this item.");
+    }
+    
+    if(item_name) item.item_name = item_name;
+    if(item_description) item.item_description = item_description;
+    if(item_price) item.item_price = item_price;
+    if(item_category) {
+      const checkCategory = await Category.findOne({category_name: item_category});
+      if (!checkCategory) {
+        return res.status(400).send("Invalid category.");
+      }
+      item.item_category = checkCategory._id;
+      item.item_category_name = item_category;
+    }
+    if(item_subcategory) {
+      const checkSubcategory = await SubCategory.findOne({subCategory_name: item_subcategory});
+      if (!checkSubcategory) {
+        return res.status(400).send("Invalid subcategory.");
+      }
+      item.item_subcategory = checkSubcategory._id;
+      item.item_subcategory_name = item_subcategory;
+    }
+    if (item_location) {
+      if (item_location.city) {
+        const checkCity = await City.findOne({city_name: item_location.city});
+        if (!checkCity) {
+          return res.status(400).send("Invalid city.");
+        }
+        item.item_location.city = checkCity._id;
+        item.item_location.city_name = item_location.city;
+      }
+      if (item_location.area) {
+        const checkArea = await City.findOne({areas: {$in: [item_location.area]}});
+        if (!checkArea) {
+          return res.status(400).send("Invalid area.");
+        }
+        item.item_location.area = item_location.area;
+      }
+      if (item_location.latitude) item.item_location.latitude = item_location.latitude;
+      if (item_location.longitude) item.item_location.longitude = item_location.longitude;
+    }
+
+    await item.save();
+
+    res.status(200).send({item, message: "Item modified successfully."});
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occurred while modifying the item.");
+  }
+}
+
+
 module.exports = {
   getUserItems,
-  createItem
+  createItem,
+  modifyItem
 }
