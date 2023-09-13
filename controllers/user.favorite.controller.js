b4dc84de787bc93c1d86b6b257d0c99a3b7bb836
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-13 09:21:34 +0300

    added addFavorite controller to user favorite controller

--- controllers/user.favorite.controller.js
+++ controllers/user.favorite.controller.js
@@ -20,6 +20,36 @@ const getFavorites = async (req, res) => {
 };
 
 
+const addFavorite = async (req, res) => {
+  const { item_id } = req.params;
+  const { _id: user_id } = req.user;
+
+  try {
+    const user = await User.findById(user_id);
+
+    if (!user) {
+      return res.status(404).send("User not found.");
+    }
+
+    const existingFavorite = user.user_favorites.find(favorite => favorite.item_id.toString() === item_id);
+
+    if (existingFavorite) {
+      return res.status(400).send("Item already in favorites.");
+    }
+
+    user.user_favorites.push({ item_id });
+
+    await user.save();
+
+    res.status(200).send({ message: "Item added to favorites successfully." });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while adding the item to favorites.");
+  }
+};
+
+
 module.exports = {
-  getFavorites
+  getFavorites,
+  addFavorite
 }
