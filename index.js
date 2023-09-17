3af1356160d1cbb7bd87534aa855a9c0fd285871
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-17 17:51:46 +0300

    connected the socket server to port 4000

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ app.use(express.json());
 
 const server = http.createServer(app);
 
+const SocketPort = 4000;
+server.listen(SocketPort, () => {
+  console.log(`Server is running on port: ${PORT}`);
+});
+
 
 
 const mongooseConnect = require("./configs/mongoDB.connect");
