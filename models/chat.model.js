cbd2cb09823a1ebee3607f22251fbd7117d10182
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-17 18:00:35 +0300

    added message schema to chat model

--- models/chat.model.js
+++ models/chat.model.js
@@ -1,2 +1,16 @@
 const mongoose = require('mongoose');
 const User = require('./user.model');
+
+
+const messageSchema = new mongoose.Schema({
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+  sender_name: String,  
+  message: String,
+  message_time: {
+    type: Date,
+    default: Date.now(),
+  },
+});
