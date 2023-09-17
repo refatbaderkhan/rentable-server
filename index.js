cebcd65007e2eb4dfce0f42810cd5976972049b5
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-17 17:52:41 +0300

    started io server connection

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ server.listen(SocketPort, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
 
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+});
+
 
 
 const mongooseConnect = require("./configs/mongoDB.connect");
