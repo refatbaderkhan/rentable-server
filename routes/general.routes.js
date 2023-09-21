08a6f0e111d5a99f856a362485031bc10ec0d7fc
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-21 04:46:52 +0300

    added getCities router to general routes

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -5,5 +5,6 @@ const generalController = require("../controllers/general.controller");
 router.get("/items/:user_id/:item_id", generalController.getItems)
 router.get("/profile/:user_id", generalController.getUser)
 router.get("/categories", generalController.getCategories)
+router.get("/cities", generalController.getCities)
 
 module.exports = router;
