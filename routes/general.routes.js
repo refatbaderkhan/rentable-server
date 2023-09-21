b89e77cb56421c6c3ea98e1827278be01cfde618
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-21 22:09:58 +0300

    updated getUser router in generalController

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const generalController = require("../controllers/general.controller");
 
 router.get("/items/:user_id/:item_id", generalController.getItems)
-router.get("/profile/:user_id", generalController.getUser)
+router.get("/account/:user_id?", generalController.getUser)
 router.get("/categories", generalController.getCategories)
 router.get("/cities", generalController.getCities)
 
