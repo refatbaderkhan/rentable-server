27d3ac97b6a728e94c0486fa68c795e0bb9f8eeb
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-22 09:31:30 +0300

    updated getItems API

--- controllers/general.controller.js
+++ controllers/general.controller.js
@@ -4,25 +4,14 @@ const {Item, Category, SubCategory, City} = require("../models/item.model")
 
 const getItems = async (req, res)=>{
   try {
-    const filter = {};
-    const {category, subcategory, city, area, user_id, item_id} = req.query;
+    const items = await Item.find();
 
-    if (category) filter["item_subcategory.category_name"] = category;
-    if (subcategory) filter["item_subcategory.subcategory_name"] = subcategory;
-    if (city) filter["item_location.city"] = city;
-    if (area) filter["item_location.area"] = area;
-    if (user_id) filter["user_id"] = user_id;
-    if (item_id) filter["_id"] = item_id;
-    if (minPrice && maxPrice) filter["item_price"] = {$gte: minPrice, $lte: maxPrice};
-    if (minPrice) filter["item_price"] = {$gte: minPrice};
-    if (maxPrice) filter["item_price"] = {$lte: maxPrice};
-
-    const items = await Item.find(filter);
     res.status(200).send(items);
   } catch (error) {
     res.status(500).send("An error occurred while getting the items.");
   }
 }
+
 
 
 const searchItems = async (req, res)=>{
