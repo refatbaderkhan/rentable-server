58d4d842f8e2e05c816213ef24ea343351721dcf
refatbaderkhan <refatbaderkhan@gmail.com>
2023-09-25 01:47:00 +0300

    added getItem router to general routes

--- routes/general.routes.js
+++ routes/general.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const generalController = require("../controllers/general.controller");
 
 router.get("/items", generalController.getItems)
+router.get("/item/:item_id", generalController.getItem)
 router.get("/account/:user_id?", generalController.getUser)
 router.get("/categories", generalController.getCategories)
 router.get("/cities", generalController.getCities)
